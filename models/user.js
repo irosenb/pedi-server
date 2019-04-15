1896f0f1da4c3975ca89f4c203afc578a223d6aa
Isaac Rosenberg <irosenb7@gmail.com>
2019-04-15 10:23:04 -0400

    User.create, use random string for session token instead of jwt,

--- models/user.js
+++ models/user.js
@@ -1,15 +1,15 @@
-const { Client } = require('pg'); 
+const { Client } = require('pg');
 
-var User = {} 
+var User = {}
 
-User.create = function(email, first_name, last_name, password, is_driver, callback) {
+User.create = function(email, first_name, last_name, password, is_driver, token, callback) {
   const client = User.connection();
 
-  var text = "INSERT INTO Users(email, first_name, last_name, password, is_driver) VALUES ($1, $2, $3, $4, $5)"
-  var values = [email, first_name, last_name, password, is_driver];
+  var text = "INSERT INTO Users(email, first_name, last_name, password, is_driver, auth_token) VALUES ($1, $2, $3, $4, $5, $6)"
+  var values = [email, first_name, last_name, password, is_driver, token];
   client.query(text, values, (err, results) => {
     if (err) {
-      console.log(err); 
+      console.log(err);
       callback(null, err);
     } else {
       callback(results.rows, null);
@@ -18,24 +18,42 @@ User.create = function(email, first_name, last_name, password, is_driver, callba
 }
 
 User.all = function(callback) {
-  const client = User.connection(); 
+  const client = User.connection();
 
   var text = "SELECT * FROM Users";
   client.query(text, function(err, results) {
     if (err) {
-      callback(null, err); 
+      callback(null, err);
     } else {
       callback(results.rows, null);
     }
-  }) 
+  })
+}
+
+User.find_by_session_token = function(token, callback) {
+  const client = User.connection();
+  const query = {
+    text: "SELECT * FROM Users WHERE auth_token = $1",
+    values: [token]
+  }
+  client.query(query, function(err, res) {
+    if (err) {
+      console.log(err.stack);
+      callback(null);
+    } else {
+      callback(res.rows[0]);
+    }
+  })
+
 }
 
 User.connection = function() {
-  const client = new Client(); 
-  client.connect(); 
+  const client = new Client();
+  client.connect();
 
   return client
 }
+
 
 for (var k in User) {
   exports[k] = User[k];
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var crypto = require('crypto');
 var User = require('../models/user.js');
 
 const fs = require('fs');
-var jwt = require('jsonwebtoken');
 
-const bcrypt = require('bcrypt'); 
+const bcrypt = require('bcrypt');
 const saltRounds = 12;
 
 /* GET users listing. */
@@ -15,39 +14,38 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  var email = req.body.email; 
+  var email = req.body.email;
   var first_name = req.body.first_name;
-  var last_name = req.body.last_name; 
-  var is_driver = req.body.is_driver || false
-  console.log("is driver: " + is_driver)
-  const saltRounds = 10 
+  var last_name = req.body.last_name;
+  var is_driver = req.body.is_driver || false;
 
+  const saltRounds = 10
   bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
     if (err) {
       res.status(500);
       return;
     }
 
-    // var privateKey = fs.readFileSync('~/.ssh/id_rsa');
-    var token = jwt.sign({email: email}, 'shhhhh');
+    var token = crypto.randomBytes(20).toString('hex');
+
     console.log(token);
     var password = hash;
 
-    User.create(email, first_name, last_name, password, is_driver, function(result, err) {
+    User.create(email, first_name, last_name, password, is_driver, token, function(result, err) {
       if (result) {
         var data = {}
 
         data["first_name"] = result["first_name"];
         data["last_name"] = result["last_name"];
         data["email"] = result["email"];
-        data["auth_token"] = token; 
-
+        data["auth_token"] = token;
+        data["is_driver"] = result["is_driver"];
         res.send(200, data);
-        return; 
+        return;
       }
 
-      res.send(500); 
-    }) 
+      res.send(500);
+    })
   })
 })
 
