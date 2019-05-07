4f5234c4d2b48fea8582223d15e786fd157e7cc4
Isaac Rosenberg <irosenb7@gmail.com>
2019-05-07 14:51:21 -0400

    console log

--- routes/users.js
+++ routes/users.js
@@ -63,7 +63,8 @@ router.post('/credit_card', function (req, res, next) {
 
   User.find_by_session_token(session_token, function(user) {
     var token = req.body.token;
-
+    console.log(user);
+    
     User.set_customer_and_credit_card(token, user, function(result, err) {
       if (result) {
         res.status(200).send(result)
